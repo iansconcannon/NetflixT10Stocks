fecc726b2ad2d512b51301be63ee5d5d562a063b
Ian Concannon <iansc@umich.edu>
2023-04-21 00:09:28 -0400

    planned out data anlyzation part

--- analyze.py
+++ analyze.py
@@ -9,9 +9,21 @@ cur = conn.cursor()
 
 
 def make_stock_chart(conn, cur): 
-    data = []
-    cur.execute('SELECT NetflixTweets.datetime, NetflixTweets.tweets, NetflixStocks.open, NetflixStocks.close, NetflixStocks.high, NetflixStocks.low FROM NetflixTweets JOIN NetflixStocks ON NetflixTweets.datetime = NetflixStocks.datetime')
-    for row in cur:
-        data.append(row)
-    conn.commit()
+    # make candlestick chart
+    # 4 days 1 - 4:
+    # for each day date chart, JOIN stock chart on timeID = timeID, time dateID = date dateID WHERE date day = NUMBER:
+    # THEN make chart (1 of 4) and put it in a certain part.
+    return
+    
+
+
+def make_volatility_chart(conn, cur):
+    # make chart that records volatility of the tweets vs ABSOLUTE DIFFERENCE OF THE STOCKS OPEN AND CLOSE
+    # use AVG function to get AVG tweets and compare amount of tweets to avg (absolute value)
+    # make separate figure to split by days
+    return
+
+def make_pos_neg_volatility_chart(conn, cur):
+    # same as above excpet no absolute value
+    return
 
